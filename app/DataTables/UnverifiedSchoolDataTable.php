56aabf21e381c131bf93ba17a5467dbf4bb0ae63
klanjabrik <meirza.arson@sebangsa.com>
2020-02-05 17:25:38 +0700

    back rev

--- app/DataTables/UnverifiedSchoolDataTable.php
+++ app/DataTables/UnverifiedSchoolDataTable.php
@@ -74,8 +74,8 @@ class UnverifiedSchoolDataTable extends DataTable
             ->selectRaw('schools.*, users.name AS assignee_name, levels.name AS level_name, CONCAT(cities.name, ", ", provinces.name) AS city_name')
             ->leftjoin('users', 'users.id', '=', 'schools.assignee')
             ->join('levels', 'levels.id', '=', 'schools.level_id')
-            ->leftjoin('cities', 'cities.id', '=', 'schools.city_id')
-            ->leftjoin('provinces', 'provinces.id', '=', 'cities.province_id')
+            ->join('cities', 'cities.id', '=', 'schools.city_id')
+            ->join('provinces', 'provinces.id', '=', 'cities.province_id')
             ->whereNull('schools.verified_at');
     }
 
